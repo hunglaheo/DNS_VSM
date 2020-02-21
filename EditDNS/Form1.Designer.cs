300b562457dc2cde94a86ab9a46ef46b907b93b3
hunglaheo <sf.zing.vn@gmail.com>
2020-02-21 16:38:18 +0700

    Update tính năng

--- EditDNS/Form1.Designer.cs
+++ EditDNS/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -44,6 +45,7 @@
             // 
             // button2
             // 
+            this.button2.Enabled = false;
             this.button2.Location = new System.Drawing.Point(164, 58);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
@@ -52,16 +54,29 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Trạng thái";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(297, 118);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Close);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +84,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- EditDNS/Form1.cs
+++ EditDNS/Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Management;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace EditDNS
             InitializeComponent();
 
         }
+        public bool Start_active = false;
         public void ExecuteAsAdmin(string caulenh)
         {
             Process proc = new Process();
@@ -33,36 +35,73 @@ namespace EditDNS
             proc.Start();
 
         }
-        public static List<string> GetNicList() {
+        public static NetworkInterface[] GetNicList() {
             NetworkInterface[] Nic = NetworkInterface.GetAllNetworkInterfaces();
-            List<string> Nic_out = new List<string>();
-            foreach (NetworkInterface item in Nic)
+            return Nic;
+        }
+        public static List<string> GetNicList1()
+        {
+            List<string> Nic_name = new List<string>();
+            NetworkInterface[] Nic = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var item in Nic)
             {
-                Nic_out.Add(item.Name);
+                Nic_name.Add(item.Name);
             }
-            
-            return Nic_out;
+            return Nic_name;
         }
         private void button1_Click(object sender, EventArgs e)
         {
             var List_name = GetNicList();
-            int count = List_name.Count;
-            for (int i = 0; i < count; i++)
+            //for (int i = 0; i < count; i++)
+            //{
+
+            //    this.ExecuteAsAdmin("interface ip set dns "+List_name[i]+ " static 52.77.28.202");
+            //}
+            foreach (NetworkInterface item in List_name)
             {
-                this.ExecuteAsAdmin("interface ip set dns "+List_name[i]+ " static 52.77.28.202");
+                var Ip_DNS = item.NetworkInterfaceType;
+                IPInterfaceProperties adapterProperties = item.GetIPProperties();
+                IPAddressCollection dnsServers = adapterProperties.DnsAddresses;
+                if (dnsServers.Count > 0)
+                {
+                    Console.WriteLine(item.Description);
+                    foreach (IPAddress dns in dnsServers)
+                    {
+                        if (dns.ToString() != "52.77.28.202")
+                        {
+                            this.ExecuteAsAdmin("interface ip set dns " + item.Name + " static 52.77.28.202");
+                        }
+                    }
+                }
             }
-            
-            
+            this.Start_active = true;
+            button2.Enabled = true;
+            button1.Enabled = false;
+            label1.Text = "Kích hoạt thành công.";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            var List_name = GetNicList();
+            var List_name = GetNicList1();
             int count = List_name.Count;
             for (int i = 0; i < count; i++)
             {
                 this.ExecuteAsAdmin("interface ip set dns " + List_name[i] + " dhcp");
             }
+            this.Start_active = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label1.Text = "Vô hiệu hóa thành công.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close(object sender, FormClosedEventArgs e)
+        {
+
         }
     }
 }
